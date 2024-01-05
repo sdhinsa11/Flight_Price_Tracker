c4b7e5e911264d71f5e1a65bf700efcbfe3ad95b
SDHINSA <sohanad@icloud.com>
2024-01-04 22:43:52 -0700

    first commit

---
+++ from bs4 import BeautifulSoup.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+import requests
+
+#requesting information from a specific website, (like person that requests information)
+
+
+html_text = requests.get('') # type this in and put URL in brackets too show the library you want too request 
+                            # can name the variable whatever 
